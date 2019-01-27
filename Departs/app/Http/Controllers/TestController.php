c4c202b4db57fc0f0cde359124da6c9e6a14ff73
濱田裕史 <uziu2hahaha@gmail.com>
2019-01-27 15:54:15 +0900

    メソッド分け noteの追加

--- Departs/app/Http/Controllers/TestController.php
+++ Departs/app/Http/Controllers/TestController.php
@@ -9,35 +9,78 @@ use Illuminate\Support\Facades\Storage;
 class TestController extends Controller
 {
     public function index() {
+        $this->note();
+        $this->googleNews();
+        $this->blogos();
+    }
 
-        // GoogleNews
+    private function note() {
+        $xml = simplexml_load_file('https://note.mu/hashtag/%E7%99%BE%E8%B2%A8%E5%BA%97/rss');
+
+        $nameSpaces = $xml->getNamespaces(true);
+        $items = $xml->channel->item;
+        
+        foreach($items as $item) {
+            $article = [];
+            $article['title'] = (string)$item->title;
+            $article['url'] =(string) $item->link;
+            $article['guid'] = (string)$item->guid;
+            $article['date'] = strtotime((string)$item->pubDate);
+            $article['source'] = (string)$item->children($nameSpaces['note'])->creatorName;
+            $article['image_url'] = (string)$item->children($nameSpaces['note'])->creatorImage;
+            $item_array[] = $article;
+        }
+        $json = json_encode($item_array, JSON_UNESCAPED_SLASHES|JSON_UNESCAPED_UNICODE);
+        Storage::disk('s3')->put('Note.json', $json,'public');
+    }
+
+    private function googleNews() {
         $xml = simplexml_load_file('https://news.google.com/rss/search?q=%E7%99%BE%E8%B2%A8%E5%BA%97&hl=ja&gl=JP&ceid=JP:ja');
 
         $nameSpaces = $xml->getNamespaces(true);
         $items = $xml->channel->item;
         foreach($items as $item) {
+            $article = [];
+            $article['title'] = (string)$item->title;
+            $article['url'] =(string) $item->link;
+            $article['guid'] = (string)$item->guid;
+            $article['date'] = strtotime((string)$item->pubDate);
+            $article['description'] = (string)$item->description;
+            $article['source'] = (string)$item->source;
+
             if (isset($item->children($nameSpaces['media'])->content)) {
 
                 $url = (string)$item->children($nameSpaces['media'])->content->attributes()->url;
                 $item = json_decode(json_encode($item), true);
-                $item['image_url'] = $url;
+                $article['image_url'] = $url;
+            } else {
+                $article['image_url'] = "";
             }
-            $item_array[] = $item;
+            $item_array[] = $article;
         }
 
-        $json = json_encode($item_array);
+        $json = json_encode($item_array, JSON_UNESCAPED_SLASHES|JSON_UNESCAPED_UNICODE);
         Storage::disk('s3')->put('GoogleNews.json', $json,'public');
+    }
 
-        // Blogos
+    private function blogos() {
         $xml = simplexml_load_file('https://blogos.com/feed/tag/%E7%99%BE%E8%B2%A8%E5%BA%97/');
 
         $items = $xml->channel->item;
+        $item_array = [];
         foreach($items as $item) {
-            $item_array[] = $item;
+            $article = [];
+            $article['title'] = (string)$item->title;
+            $article['url'] =(string) $item->link;
+            $article['guid'] = (string)$item->guid;
+            $article['date'] = strtotime((string)$item->pubDate);
+            $article['description'] = (string)$item->description;
+            $article['source'] = (string)$item->source;
+            $article['image_url'] = "";
+            $item_array[] = $article;
         }
-        $json = json_encode($item_array);
 
+        $json = json_encode($item_array, JSON_UNESCAPED_SLASHES|JSON_UNESCAPED_UNICODE);
         Storage::disk('s3')->put('Blogos.json', $json,'public');
-
     }
 }
