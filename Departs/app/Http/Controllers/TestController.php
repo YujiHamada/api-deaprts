e1fe3a5e9cfc51a78cdb78a9a1ada8d9af55513b
濱田裕史 <uziu2hahaha@gmail.com>
2019-03-16 20:40:15 +0900

    コンテンツ一旦完了

--- Departs/app/Http/Controllers/TestController.php
+++ Departs/app/Http/Controllers/TestController.php
@@ -41,23 +41,21 @@ class TestController extends Controller
     }
 
     public function index() {
-//        $this->note();
-//        $this->blogos();
-//        $this->shokuhin();
-//        $this->n();
 
-        $this->scAndDepartsTab();
+        $this->departsTab();
+        $this->scTab();
         $this->eventChecker();
         $this->apparelTab();
-        $this->foods();
-        $this->ryutsuu();
+        $this->foodsTab();
+        $this->ryusuutab();
+        $this->cosmeticsTab();
+        $this->interiorTab();
     }
 
-    private function scAndDepartsTab() {
+    private function departsTab() {
         $urls = [
-            'https://news.google.com/rss/search?q=%E7%99%BE%E8%B2%A8%E5%BA%97&hl=ja&gl=JP&ceid=JP:ja',
-            'https://news.google.com/rss/search?q=%E4%B8%89%E8%B6%8A%E4%BC%8A%E5%8B%A2%E4%B8%B9%20%E3%81%9D%E3%81%94%E3%81%86%E8%A5%BF%E6%AD%A6%20%E5%A4%A7%E4%B8%B8%E6%9D%BE%E5%9D%82%E5%B1%8B%20%E9%AB%98%E5%B3%B6%E5%B1%8B&hl=ja&gl=JP&ceid=JP:ja',
-            'https://news.google.com/rss/search?q=%E3%82%B7%E3%83%A7%E3%83%83%E3%83%94%E3%83%B3%E3%82%B0%E3%82%BB%E3%83%B3%E3%82%BF%E3%83%BC&hl=ja&gl=JP&ceid=JP:ja',
+            'https://news.google.com/rss/search?q=%E7%99%BE%E8%B2%A8%E5%BA%97&hl=ja&gl=JP&ceid=JP:ja', //百貨店
+            'https://news.google.com/rss/search?q=%E4%B8%89%E8%B6%8A%E4%BC%8A%E5%8B%A2%E4%B8%B9%20%E3%81%9D%E3%81%94%E3%81%86%E8%A5%BF%E6%AD%A6%20%E5%A4%A7%E4%B8%B8%E6%9D%BE%E5%9D%82%E5%B1%8B%20%E9%AB%98%E5%B3%B6%E5%B1%8B&hl=ja&gl=JP&ceid=JP:ja', // leading departs
         ];
 
         $item_array = [];
@@ -68,7 +66,52 @@ class TestController extends Controller
         $item_array = $this->sotrByDate($item_array);
         $item_array = array_slice($item_array, 0, 100);
         $json = json_encode($item_array, JSON_UNESCAPED_SLASHES|JSON_UNESCAPED_UNICODE);
-        Storage::disk('s3')->put('GoogleNews.json', $json,'public');
+        Storage::disk('s3')->put('Departs.json', $json,'public');
+    }
+
+    private function scTab() {
+        $urls = [
+            'https://news.google.com/rss/search?q=%E3%82%B7%E3%83%A7%E3%83%83%E3%83%94%E3%83%B3%E3%82%B0%E3%82%BB%E3%83%B3%E3%82%BF%E3%83%BC&hl=ja&gl=JP&ceid=JP:ja', // SC
+        ];
+        $item_array = [];
+        foreach ($urls as $url) {
+            $item_array = array_merge($item_array, $this->googleNews($url));
+        }
+
+        $item_array = $this->sotrByDate($item_array);
+        $item_array = array_slice($item_array, 0, 100);
+        $json = json_encode($item_array, JSON_UNESCAPED_SLASHES|JSON_UNESCAPED_UNICODE);
+        Storage::disk('s3')->put('SC.json', $json,'public');
+    }
+
+    private function cosmeticsTab() {
+        $urls = [
+            'https://news.google.com/rss/search?q=%E5%8C%96%E7%B2%A7%E5%93%81&hl=ja&gl=JP&ceid=JP:ja', // 化粧品
+        ];
+        $item_array = [];
+        foreach ($urls as $url) {
+            $item_array = array_merge($item_array, $this->googleNews($url));
+        }
+
+        $item_array = $this->sotrByDate($item_array);
+        $item_array = array_slice($item_array, 0, 100);
+        $json = json_encode($item_array, JSON_UNESCAPED_SLASHES|JSON_UNESCAPED_UNICODE);
+        Storage::disk('s3')->put('Cosmetics.json', $json,'public');
+    }
+
+    private function interiorTab() {
+        $urls = [
+            'https://news.google.com/rss/search?q=%E3%82%A4%E3%83%B3%E3%83%86%E3%83%AA%E3%82%A2&hl=ja&gl=JP&ceid=JP:ja', // インテリア
+        ];
+        $item_array = [];
+        foreach ($urls as $url) {
+            $item_array = array_merge($item_array, $this->googleNews($url));
+        }
+
+        $item_array = $this->sotrByDate($item_array);
+        $item_array = array_slice($item_array, 0, 100);
+        $json = json_encode($item_array, JSON_UNESCAPED_SLASHES|JSON_UNESCAPED_UNICODE);
+        Storage::disk('s3')->put('Interior.json', $json,'public');
     }
 
     private function apparelTab() {
@@ -77,13 +120,14 @@ class TestController extends Controller
         $item_array = array_merge($item_array, $this->senken());
         $item_array = array_merge($item_array, $this->topseller());
         $item_array = array_merge($item_array, $this->apparelWeb());
-
+        $item_array = array_merge($item_array, $this->fashionSnap());
         $item_array = $this->sotrByDate($item_array);
+
         $json = json_encode($item_array, JSON_UNESCAPED_SLASHES|JSON_UNESCAPED_UNICODE);
         Storage::disk('s3')->put('Apparel.json', $json, 'public');
     }
 
-    private function foods() {
+    private function foodsTab() {
         $urls = [
             'https://news.google.com/rss/search?q=%E3%83%87%E3%83%91%E5%9C%B0%E4%B8%8B&hl=ja&gl=JP&ceid=JP:ja',
             'https://news.google.com/rss/search?q=%E9%A3%9F%E5%93%81&hl=ja&gl=JP&ceid=JP:ja',
@@ -102,6 +146,14 @@ class TestController extends Controller
         Storage::disk('s3')->put('Foods.json', $json,'public');
     }
 
+    private function ryusuutab() {
+        $item_array = [];
+        $item_array = array_merge($item_array, $this->ryutsuu());
+        $item_array = array_merge($item_array, $this->shogyokai());
+        $json = json_encode($item_array, JSON_UNESCAPED_SLASHES|JSON_UNESCAPED_UNICODE);
+        Storage::disk('s3')->put('Ryutsuu.json', $json, 'public');
+    }
+
     private function ryutsuu() {
         $xml = simplexml_load_file('https://www.ryutsuu.biz/feed');
         $items = $xml->channel->item;
@@ -117,8 +169,26 @@ class TestController extends Controller
             $item_array[] = $article;
         }
 
-        $json = json_encode($item_array, JSON_UNESCAPED_SLASHES|JSON_UNESCAPED_UNICODE);
-        Storage::disk('s3')->put('Ryutsuu.json', $json, 'public');
+        return $item_array;
+    }
+
+    private function shogyokai() {
+        $xml = simplexml_load_file('http://shogyokai.jp/list/feed/rss');
+        $items = $xml->channel->item;
+        $nameSpaces = $xml->getNamespaces(true);
+
+        foreach($items as $item) {
+            $article = [];
+            $article['title'] = (string)$item->title;
+            $article['url'] =(string) $item->link;
+            $article['guid'] = (string)$item->guid;
+            $article['date'] = strtotime((string)$item->pubDate);
+            $article['source'] = "商業界オンライン";
+            $article['image_url'] = (string)$item->children($nameSpaces['media'])->thumbnail->attributes()->url;
+            $item_array[] = $article;
+        }
+
+        return $item_array;
     }
 
     private function wwd() {
@@ -364,6 +434,36 @@ class TestController extends Controller
             }
             $item_array[] = $article;
         }
+        return $item_array;
+    }
+
+    private function fashionSnap() {
+
+        $xml = simplexml_load_file("https://www.fashionsnap.com/rss.xml");
+        $items = $xml->channel->item;
+        $nameSpaces = $xml->getNamespaces(true);
+        $item_array = [];
+        foreach($items as $item) {
+            $article = [];
+            $article['title'] = (string)$item->title;
+            $article['url'] =(string) $item->link;
+            $article['guid'] = (string)$item->guid;
+            $article['date'] = strtotime((string)$item->pubDate);
+            $article['source'] = "FASHIONSNAP.COM";
+
+            $pattern = '/<img.*?src\s*=\s*[\"|\'](.*?)[\"|\'].*?>/i';
+            preg_match_all($pattern, (string)$item->description, $images);
+            $article['image_url'] = "";
+            if (isset($images[1])) {
+                foreach ($images[1] as $image)
+                    if (! empty($image)) {
+                        $article['image_url'] = $image;
+                        break;
+                    }
+            }
+            $item_array[] = $article;
+        }
+        
         return $item_array;
     }
 
